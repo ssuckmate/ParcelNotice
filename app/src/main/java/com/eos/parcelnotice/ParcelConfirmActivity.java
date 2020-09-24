ae91d80c2e6ce9263891c2f3462aab96dbac66e8
WickedSejin <ihavebravelife@gmail.com>
2020-09-24 21:09:58 +0900

    androidx toolbar 적용, ParcelConfirmActivity Initiated

--- app/src/main/java/com/eos/parcelnotice/ParcelConfirmActivity.java
+++ app/src/main/java/com/eos/parcelnotice/ParcelConfirmActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ParcelConfirmActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_parcel_confirm);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
