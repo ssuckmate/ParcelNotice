47ca2f121419b25c39eb439fdf3e3ff64e0a18fc
WickedSejin <ihavebravelife@gmail.com>
2020-09-24 22:02:45 +0900

    Main에 버튼 초기화, ParcelConfirm 액티비티 연결

--- app/src/main/java/com/eos/parcelnotice/MainActivity.java
+++ app/src/main/java/com/eos/parcelnotice/MainActivity.java
@@ -1,15 +1,30 @@
 package com.eos.parcelnotice;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnParcelConfirm, btnLaundryConfirm, btnRewardConfirm, btnNoticeConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnParcelConfirm = findViewById(R.id.button_main_parcel);
+        btnLaundryConfirm = findViewById(R.id.button_main_laundry_room);
+        btnRewardConfirm = findViewById(R.id.button_main_reward);
+        btnNoticeConfirm = findViewById(R.id.button_main_notice);
+
+        btnParcelConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ParcelConfirmActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
