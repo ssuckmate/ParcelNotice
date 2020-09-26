771c94437e9a49e4368dce0e2f5a0613dd7d806e
jangbogyeong <jbok6825@naver.com>
2020-09-26 15:43:40 +0900

    Added Notice Activy and its toolbar

--- app/src/main/java/com/eos/parcelnotice/MainActivity.java
+++ app/src/main/java/com/eos/parcelnotice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.eos.parcelnotice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
