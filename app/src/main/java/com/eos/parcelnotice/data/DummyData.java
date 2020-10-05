118471217c6e2d3cd8f2755480ef46a1461a1ca4
jangbogyeong <jbok6825@naver.com>
2020-10-05 23:12:20 +0900

    RewardActivity completion

--- app/src/main/java/com/eos/parcelnotice/data/DummyData.java
+++ app/src/main/java/com/eos/parcelnotice/data/DummyData.java
@@ -11,4 +11,6 @@ public class DummyData {
             add(new ParcelData(new Date(2020,9,25), "Sejin", "Hello"));
         }
     };
+
+    public static String Notice = "1. 복도에 우산 치우기\n2. 점호 후에 조용히 하기";
 }
