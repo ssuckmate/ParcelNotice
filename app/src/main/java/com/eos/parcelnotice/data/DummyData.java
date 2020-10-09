0d0c7ffd42c54be430e9b083580109723547ae6c
jangbogyeong <jbok6825@naver.com>
2020-10-10 00:09:27 +0900

    아 checkout 안했다...ㅜㅜㅜㅜ

--- app/src/main/java/com/eos/parcelnotice/data/DummyData.java
+++ app/src/main/java/com/eos/parcelnotice/data/DummyData.java
@@ -13,4 +13,22 @@ public class DummyData {
     };
 
     public static String Notice = "1. 복도에 우산 치우기\n2. 점호 후에 조용히 하기";
+
+    public static ArrayList<RewardData> plusPoints = new ArrayList<RewardData>(){
+        {
+            add(new RewardData(new Date(2020, 10, 1), 2, "벌레 잡음"));
+            add(new RewardData(new Date(2020, 10, 3), 3, "생활실 상태 양호"));
+            add(new RewardData(new Date(2020, 10, 5), 2, "분실물 신고"));
+
+        }
+    };
+
+    public static ArrayList<RewardData> minusPoints = new ArrayList<RewardData>(){
+        {
+            add(new RewardData(new Date(2020, 10, 2), 2, "복장불량"));
+            add(new RewardData(new Date(2020, 10, 4), 3, "친구와 싸움"));
+        }
+    };
+
+
 }
