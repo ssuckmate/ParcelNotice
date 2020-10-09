69376649f2c62a9340396ba5612e9d76bd0ec260
siun <siun0816@gmail.com>
2020-10-09 18:42:11 +0900

    auto login

--- app/src/main/java/com/eos/parcelnotice/LoginActivity.java
+++ app/src/main/java/com/eos/parcelnotice/LoginActivity.java
@@ -3,10 +3,12 @@ package com.eos.parcelnotice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -26,6 +28,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText editTextID, editTextPassword;
     private CheckBox checkBoxAutoLogin;
     private SessionCallBack  sessionCallBack;
+    private Boolean loginCheck =false;
+    private SharedPreferences pref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +44,17 @@ public class LoginActivity extends AppCompatActivity {
         editTextPassword = findViewById(R.id.editText_login_password);
         checkBoxAutoLogin = findViewById(R.id.checkbox_login_autoLogin);
         sessionCallBack = new SessionCallBack();
-        Session.getCurrentSession().addCallback(sessionCallBack);
-        //Session.getCurrentSession().checkAndImplicitOpen(); //자동로그인
+        pref = getSharedPreferences("setting",0);
+        editor = pref.edit();
 
-        if(checkBoxAutoLogin.isChecked() == true){
-            //자동로그인
+        Session.getCurrentSession().addCallback(sessionCallBack);
+        //Session.getCurrentSession().checkAndImplicitOpen(); //카카오톡 로그인 한 번 해놓으면 이후 자동로그인 (바로 메인 페이지)
+
+        //자동로그인
+        if(pref.getBoolean("autoLogin", false)){
+            editTextID.setText(pref.getString("id",""));
+            editTextPassword.setText(pref.getString("password",""));
+            checkBoxAutoLogin.setChecked(true);
         }
 
         btnKakaoLogin.setOnClickListener(new View.OnClickListener() {
@@ -60,22 +71,46 @@ public class LoginActivity extends AppCompatActivity {
                 String password = editTextPassword.getText().toString();
 
                 //일치하는 회원이 있는지 확인
+                Boolean validation = loginValidation(id,password);
 
                 //로그인 성공시
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                if(validation) {
+                    if(loginCheck){
+                        editor.putString("id",id);
+                        editor.putString("password",password);
+                        editor.putBoolean("autoLogin",true);
+                        editor.commit();
+                    }
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("userID", id);
+                    startActivity(intent);
+                }
 
-                //로그인 실패시
-                /*Toast.makeText(getApplicationContext(),"아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
-                return;*/
+                else {
+                    Toast.makeText(getApplicationContext(), "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-/*        btnRegister.setOnClickListener(new View.OnClickListener() {
+        checkBoxAutoLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    loginCheck = true;
+                }
+                else{
+                    loginCheck = false;
+                    editor.clear();
+                    editor.commit();
+                }
+            }
+        });
+
+        /*
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+                //회원가입
             }
         });*/
     }
@@ -91,6 +126,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Session.getCurrentSession().removeCallback(sessionCallBack);
+    }
+
+    private boolean loginValidation(String id, String password){
+        if(id.equals("1")) return false;
+        return true;
     }
 
     private class SessionCallBack implements ISessionCallback {
@@ -119,8 +159,13 @@ public class LoginActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(MeV2Response result) {
                     // 로그인 성공
+
+                    //처음 로그인하는 계정인 경우 ->회원가입
+
+
+                    //처음 로그인하는 게 아닌 경우
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                    //intent.putExtra("id",result.getKakaoAccount().getEmail());
+                    intent.putExtra("userID", result.getId());
                     startActivity(intent);
                 }
             });
