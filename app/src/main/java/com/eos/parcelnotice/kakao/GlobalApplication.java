4466b3980774de65eea930f0b79f501e2c9dcaef
siun <siun0816@gmail.com>
2020-10-10 16:13:36 +0900

    update

---
+++ app/src/main/java/com/eos/parcelnotice/kakao/GlobalApplication.java
@@ -0,0 +1,29 @@
+package com.eos.parcelnotice.kakao;
+
+import android.app.Application;
+
+import com.kakao.auth.KakaoSDK;
+
+public class GlobalApplication extends Application {
+    private static GlobalApplication instance;
+
+    public static GlobalApplication getGlobalApplicationContext(){
+        if(instance==null) {
+            throw new IllegalStateException("This Application does not inherit com.kakao.GlobalApplication");
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        KakaoSDK.init(new KakaoSDKAdapter());
+    }
+
+    public void onTerminate(){
+        super.onTerminate();
+        instance = null;
+    }
+}
